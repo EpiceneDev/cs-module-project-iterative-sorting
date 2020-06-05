edb9a1378109da27750714ac7e2504953e18e005
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-05 16:37:58 -0700

    searching debugged

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
@@ -15,6 +17,19 @@ def binary_search(arr, target):
     low = 0
     high = len(arr)-1
 
-    
+    while low <= high:
+
+        middle = (low + high)//2
+
+        if target < arr[middle]:
+            high = middle - 1
+
+        elif target > arr[middle]:
+            low = middle + 1
+
+        # if target = arr[middle]
+        else:
+            return middle
+
 
     return -1  # not found
