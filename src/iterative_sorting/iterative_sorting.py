6d641f1ab66c2ba65710695f3fff0889ded8a732
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2020-06-04 18:52:31 -0700

    bubble sort cleaned up

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,5 +1,12 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
+    ## repeat (numOfElements - 1) times
+    #   set the first unsorted element as the minimum
+    #   for each of the unsorted elements
+    #     if element < currentMinimum
+    #       set element as new minimum
+    #   swap minimum with first unsorted position
+
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
@@ -8,22 +15,30 @@ def selection_sort(arr):
         # (hint, can do in 3 loc)
         # Your code here
         ## loop thru the next element for comparison
-        for j in range(i+1, len(arr)):
+        for j in range(i+1, len(arr)-2):
         ## compare two elements
             if arr[j] < arr[smallest_index]:
         ## rename the smallest element as smallest_index
                 smallest_index = j
         # TO-DO: swap
         # Your code here
-        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
-
+            arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
+    
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
+# TO-DO:  implement the Bubble Sort function below O(n^2)
 def bubble_sort(arr):
     # Your code here
+    ## Keep track of swaps, if no swaps end program
+    ## compare the first and second elements
+    ## if first is bigger, swap
+    for i in range(len(arr) - 1):
 
+        for j in (0, len(arr) - i - 1):
+            
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
     return arr
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,6 +9,12 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    if len(arr) == 0:
+        return -1
 
+    low = 0
+    high = len(arr)-1
+
+    
 
     return -1  # not found
